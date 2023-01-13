dddfc848750d6b9d6430708f77ae005031656767
eellda <tree500253@gmail.com>
2023-01-13 17:45:23 +0900

    0113 CRUD project

---
+++ app/src/main/java/bitcamp/bootapp/vo/Delivery.java
@@ -0,0 +1,49 @@
+package bitcamp.bootapp.vo;
+
+public class Delivery {
+
+  public int no;
+  public int tel;
+  public String name;
+  public String addr;
+  public String msg;
+  public String createdDate;
+
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public int getTel() {
+    return tel;
+  }
+  public void setTel(int tel) {
+    this.tel = tel;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getAddr() {
+    return addr;
+  }
+  public void setAddr(String addr) {
+    this.addr = addr;
+  }
+  public String getMsg() {
+    return msg;
+  }
+  public void setMsg(String msg) {
+    this.msg = msg;
+  }
+  public String getCreatedDate() {
+    return createdDate;
+  }
+  public void setCreatedDate(String createdDate) {
+    this.createdDate = createdDate;
+  }
+
+}

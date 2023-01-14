64fa1fcd36c2b1ac620417a1a8eb90f4948b0b48
eellda <tree500253@gmail.com>
2023-01-14 22:01:05 +0900

    배송 내용 수정/삭제 기능 및 페이지 추가

--- CLUD-hackerton/backend/app/src/main/java/bitcamp/bootapp/controller/DeliveryController.java
+++ CLUD-hackerton/backend/app/src/main/java/bitcamp/bootapp/controller/DeliveryController.java
@@ -2,12 +2,14 @@ package bitcamp.bootapp.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import bitcamp.bootapp.dao.DeliveryDao;
 import bitcamp.bootapp.vo.Delivery;
 
@@ -15,81 +17,92 @@ import bitcamp.bootapp.vo.Delivery;
 @RestController
 public class DeliveryController {
 
-  DeliveryDao deliveryDao = new DeliveryDao();
+	DeliveryDao deliveryDao = new DeliveryDao();
 
-  public DeliveryController(DeliveryDao deliveryDao) {
-    this.deliveryDao = deliveryDao;
-  }
+	public DeliveryController(DeliveryDao deliveryDao) {
+		this.deliveryDao = deliveryDao;
+	}
 
-  @PostMapping("/delivery")
-  public Object addDelivery(Delivery d) {
+	@PostMapping("/delivery")
+	public Object addDelivery(Delivery d) {
 
-    this.deliveryDao.insert(d);
+		this.deliveryDao.insert(d);
 
-    // 응답 결과를 담을 맵 객체 준비
-    Map<String,Object> contentMap = new HashMap<>();
-    contentMap.put("status", "success");
+		// 응답 결과를 담을 맵 객체 준비
+		Map<String,Object> contentMap = new HashMap<>();
+		contentMap.put("status", "success");
 
-    return contentMap;
-  }
+		return contentMap;
+	}
 
-  @GetMapping("/delivery/{no}")
-  public Object getDelivery(@PathVariable int no) {
+	@GetMapping("/delivery/{no}")
+	public Object getDelivery(@PathVariable int no) {
 
-    Delivery d = this.deliveryDao.findByNo(no);
+		Delivery d = this.deliveryDao.findByNo(no);
 
-    // 응답 결과를 담을 맵 객체 준비
-    Map<String,Object> contentMap = new HashMap<>();
+		// 응답 결과를 담을 맵 객체 준비
+		Map<String,Object> contentMap = new HashMap<>();
 
-    if (d == null) {
-      contentMap.put("status", "failure");
-      contentMap.put("data", "해당 번호의 택배가 없습니다.");
-    } else {
-      contentMap.put("status", "success");
-      contentMap.put("data", d);
-    }
+		if (d == null) {
+			contentMap.put("status", "failure");
+			contentMap.put("data", "해당 번호의 택배가 없습니다.");
+		} else {
+			contentMap.put("status", "success");
+			contentMap.put("data", d);
+		}
 
-    return contentMap;
-  }
+		return contentMap;
+	}
 
-  @PutMapping("/delivery/{no}")
-  public Object updateDelivery(Delivery d) {
+	@GetMapping("/delivery")
+	public Object getDeliverys() {
+		Delivery[] deliveryDao = this.deliveryDao.findAll();
 
-    Map<String,Object> contentMap = new HashMap<>();
+		Map<String, Object> contentMap = new HashMap<>();
+		contentMap.put("status", "success");
+		contentMap.put("data", deliveryDao);
 
-    Delivery old = this.deliveryDao.findByNo(d.getNo());
-    if (old == null) {
-      contentMap.put("status", "failure");
-      contentMap.put("data", "수정할 내용이 없습니다.");
-      return contentMap;
-    }
+		return contentMap;
+	}
 
-    d.setCreatedDate(old.getCreatedDate());
+	@PutMapping("/delivery/{no}")
+	public Object updateDelivery(Delivery d) {
 
-    this.deliveryDao.update(d);
+		Map<String,Object> contentMap = new HashMap<>();
 
-    contentMap.put("status", "success");
+		Delivery old = this.deliveryDao.findByNo(d.getNo());
+		if (old == null) {
+			contentMap.put("status", "failure");
+			contentMap.put("data", "수정할 내용이 없습니다.");
+			return contentMap;
+		}
 
-    return contentMap;
-  }
+		d.setCreatedDate(old.getCreatedDate());
 
-  @DeleteMapping("/delivery/{no}")
-  public Object deleteDelivery(@PathVariable int no) {
+		this.deliveryDao.update(d);
 
-    Delivery d = this.deliveryDao.findByNo(no);
+		contentMap.put("status", "success");
 
-    // 응답 결과를 담을 맵 객체 준비
-    Map<String,Object> contentMap = new HashMap<>();
+		return contentMap;
+	}
 
-    if (d == null) {
-      contentMap.put("status", "failure");
-      contentMap.put("data", "해당 번호의 택배가 없습니다.");
+	@DeleteMapping("/delivery/{no}")
+	public Object deleteDelivery(@PathVariable int no) {
 
-    } else {
-      this.deliveryDao.delete(d);
-      contentMap.put("status", "success");
-    }
+		Delivery d = this.deliveryDao.findByNo(no);
 
-    return contentMap;
-  }
+		// 응답 결과를 담을 맵 객체 준비
+		Map<String,Object> contentMap = new HashMap<>();
+
+		if (d == null) {
+			contentMap.put("status", "failure");
+			contentMap.put("data", "해당 번호의 택배가 없습니다.");
+
+		} else {
+			this.deliveryDao.delete(d);
+			contentMap.put("status", "success");
+		}
+
+		return contentMap;
+	}
 }

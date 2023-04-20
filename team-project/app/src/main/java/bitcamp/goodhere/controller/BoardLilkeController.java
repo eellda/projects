976de2da539da85f4bdf5a8a427295e57526c5e6
eellda <tree500253@gmail.com>
2023-04-20 10:29:09 +0900

    Fix RequestBody

--- team-project/app/src/main/java/bitcamp/goodhere/controller/BoardLilkeController.java
+++ team-project/app/src/main/java/bitcamp/goodhere/controller/BoardLilkeController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +32,7 @@ public class BoardLilkeController {
   }
 
   @PostMapping("/delete")
-  public ResponseEntity<String> delete(@RequestBody BoardLike boardLike) {
+  public ResponseEntity<String> delete(BoardLike boardLike) {
     boardLikeService.delete(boardLike);
     System.out.println("삭제 호출");
     return ResponseEntity.ok("success");
